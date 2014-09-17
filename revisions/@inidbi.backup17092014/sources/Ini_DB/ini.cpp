37c6f9b0b722a1c474faa0f1f9fda33901627191
asuri0n <asurion61@gmail.com>
2014-09-17 23:52:15 +0100

    update rev
    update rev

---
+++ revisions/@inidbi.backup17092014/sources/Ini_DB/ini.cpp
@@ -0,0 +1,35 @@
+#include "stdafx.h"
+
+bool Ini::Read(char* file, char* section, char* key, char* defaultValue, char* output, size_t outputSize)
+{
+	if(GetPrivateProfileStringA(section, key, defaultValue, output, outputSize, file) == NULL)
+	{
+		return false;
+	}
+
+	if(GetLastError() == 2)
+	{
+		return false;
+	}
+
+	return true;
+}
+
+bool Ini::Write(char* file, char* section, char* key, char* value)
+{
+	if(WritePrivateProfileStringA(section, key, value, file) == FALSE)
+	{
+		return false;
+	}
+
+	return true;
+}
+
+bool Ini::Deletesection(char* file, char* section) 
+{
+   if(WritePrivateProfileStructA(section, NULL, NULL, 0, file) == NULL)
+   {
+	   return false;
+   }
+   return true;
+}
---
+++ revisions/@inidbi.backup17092014/sources/Ini_DB/ini.h
@@ -0,0 +1,10 @@
+#pragma once
+
+class Ini
+{
+public:
+
+	static bool Read(char* file, char* section, char* key, char* defaultValue, char* output, size_t outputSize);
+	static bool Write(char* file, char* section, char* key, char* value);
+	static bool Deletesection(char* file, char* section);
+};
